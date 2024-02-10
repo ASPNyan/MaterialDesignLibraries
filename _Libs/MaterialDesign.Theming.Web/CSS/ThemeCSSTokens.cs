7d7d2251908b61418180f2cc047eabf2eb205863
ASPNyan <sebastian.ricketts2210@gmail.com>
2024-02-10 17:06:03 +1030

    Update .Theming.Web to 1.1.1 to add const CSS tokens to ThemeCSSTokens component/class

---
+++ _Libs/MaterialDesign.Theming.Web/CSS/ThemeCSSTokens.cs
@@ -0,0 +1,35 @@
+﻿namespace MaterialDesign.Theming.Web.CSS;
+
+public partial class ThemeCSSTokens
+{
+    public const string PrimaryCSSToken = "--md-sys-color-primary";
+    public const string OnPrimaryCSSToken = "--md-sys-color-on-primary";
+    public const string PrimaryContainerCSSToken = "--md-sys-color-primary-container";
+    public const string OnPrimaryContainerCSSToken = "--md-sys-color-on-primary-container";
+    public const string SecondaryCSSToken = "--md-sys-color-secondary";
+    public const string OnSecondaryCSSToken = "--md-sys-color-on-secondary";
+    public const string SecondaryContainerCSSToken = "--md-sys-color-secondary-container";
+    public const string OnSecondaryContainerCSSToken = "--md-sys-color-on-secondary-container";
+    public const string TertiaryCSSToken = "--md-sys-color-tertiary";
+    public const string OnTertiaryCSSToken = "--md-sys-color-on-tertiary";
+    public const string TertiaryContainerCSSToken = "--md-sys-color-tertiary-container";
+    public const string OnTertiaryContainerCSSToken = "--md-sys-color-on-tertiary-container";
+    public const string ErrorCSSToken = "--md-sys-color-error";
+    public const string ErrorContainerCSSToken = "--md-sys-color-error-container";
+    public const string OnErrorCSSToken = "--md-sys-color-on-error";
+    public const string OnErrorContainerCSSToken = "--md-sys-color-on-error-container";
+    public const string BackgroundCSSToken = "--md-sys-color-background";
+    public const string OnBackgroundCSSToken = "--md-sys-color-on-background";
+    public const string SurfaceCSSToken = "--md-sys-color-surface";
+    public const string OnSurfaceCSSToken = "--md-sys-color-on-surface";
+    public const string SurfaceVariantCSSToken = "--md-sys-color-surface-variant";
+    public const string OnSurfaceVariantCSSToken = "--md-sys-color-on-surface-variant";
+    public const string OutlineCSSToken = "--md-sys-color-outline";
+    public const string InverseOnSurfaceCSSToken = "--md-sys-color-inverse-on-surface";
+    public const string InverseSurfaceCSSToken = "--md-sys-color-inverse-surface";
+    public const string InversePrimaryCSSToken = "--md-sys-color-inverse-primary";
+    public const string ShadowCSSToken = "--md-sys-color-shadow";
+    public const string SurfaceTintCSSToken = "--md-sys-color-surface-tint";
+    public const string OutlineVariantCSSToken = "--md-sys-color-outline-variant";
+    public const string ScrimCSSToken = "--md-sys-color-scrim";
+}
