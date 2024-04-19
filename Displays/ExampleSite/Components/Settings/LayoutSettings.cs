9c27ebf93dad13c255d34f815b076d767791da58
ASPNyan <sebastian.ricketts2210@gmail.com>
2024-04-19 22:36:52 +0930

    Add layout page, update dependencies.

--- Displays/ExampleSite/Components/Settings/LayoutSettings.cs
+++ Displays/ExampleSite/Components/Settings/LayoutSettings.cs
@@ -1,10 +1,15 @@
+using System.Diagnostics.Contracts;
 using MudBlazor;
 
 namespace ExampleSite.Components.Settings;
 
-public class LayoutSettings
+public class LayoutSettings : IEquatable<LayoutSettings>
 {
-    private DrawerPosition _drawerPosition = DrawerPosition.Left;
+    private Guid Hash { get; } = Guid.NewGuid();
+    
+    private LayoutPosition _drawerPosition = LayoutPosition.Left;
+    private LayoutPosition _headerPosition = LayoutPosition.Top;
+    private Color _layoutColor = Color.Secondary;
     private bool _closeDrawerOnMainFocus = true;
     private bool _closeDrawerOnNavigate = true;
 
@@ -28,25 +33,143 @@ public class LayoutSettings
         }
     }
 
-    public required DrawerPosition DrawerPosition
+    public required LayoutPosition DrawerPosition
     {
         get => _drawerPosition;
         set
         {
+            if (value is not LayoutPosition.Left and not LayoutPosition.Right)
+                throw new ArgumentOutOfRangeException(nameof(value), value, "DrawerPosition must be ether Left or Right");
+            
             _drawerPosition = value;
+            OnUpdate?.Invoke();
+        }
+    }
+
+    public required LayoutPosition HeaderPosition
+    {
+        get => _headerPosition;
+        set
+        {
+            if (value is not LayoutPosition.Top and not LayoutPosition.Bottom)
+                throw new ArgumentOutOfRangeException(nameof(value), value, "HeaderPosition must be ether Top or Bottom");
+            
+            _headerPosition = value;
+            OnUpdate?.Invoke();
+        }
+    }
+
+    public required Color LayoutColor
+    {
+        get => _layoutColor;
+        set
+        {
+            _layoutColor = value;
             OnUpdate?.Invoke();
         }
     }
 
     public Anchor MudDrawerPosition => (Anchor)DrawerPosition;
 
-    public event Action? OnUpdate;
-}
+    public string HeaderClass
+    {
+        get
+        {
+            switch (LayoutColor)
+            {
+                case Color.Primary:
+                case Color.Secondary:
+                case Color.Tertiary:
+                    return $"{LayoutColor}-container on-{LayoutColor}-container-text".ToLowerInvariant();
+                case Color.PrimarySwapped:
+                case Color.SecondarySwapped:
+                case Color.TertiarySwapped:
+                    string color = LayoutColor.ToString().Replace("Swapped", null).ToLowerInvariant();
+                    return $"on-{color} on-{color}-container-text";
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(LayoutColor), LayoutColor, "The set LayoutColor was invalid.");
+            }
+        }
+    }
+    
+    public string DrawerClass
+    {
+        get
+        {
+            switch (LayoutColor)
+            {
+                case Color.Primary:
+                case Color.Secondary:
+                case Color.Tertiary:
+                    return $"on-{LayoutColor} on-{LayoutColor}-container-text".ToLowerInvariant();
+                case Color.PrimarySwapped:
+                case Color.SecondarySwapped:
+                case Color.TertiarySwapped:
+                    string color = LayoutColor.ToString().Replace("Swapped", null).ToLowerInvariant();
+                    return $"{color}-container on-{color}-container-text";
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(LayoutColor), LayoutColor, "The set LayoutColor was invalid.");
+            }
+        }
+    }
 
-public enum DrawerPosition
-{
-    Left = Anchor.Left,
-    Right = Anchor.Right,
-    Top = Anchor.Top,
-    Bottom = Anchor.Bottom
+    public void Update(LayoutSettings newSettings)
+    {
+        _drawerPosition = newSettings._drawerPosition;
+        _headerPosition = newSettings._headerPosition;
+        _layoutColor = newSettings._layoutColor;
+        _closeDrawerOnMainFocus = newSettings._closeDrawerOnMainFocus;
+        _closeDrawerOnNavigate = newSettings._closeDrawerOnNavigate;
+        OnUpdate?.Invoke();
+    }
+
+    [Pure]
+    public LayoutSettings Clone() => new()
+    {
+        CloseDrawerOnNavigate = CloseDrawerOnNavigate,
+        CloseDrawerOnMainFocus = CloseDrawerOnMainFocus,
+        DrawerPosition = DrawerPosition,
+        HeaderPosition = HeaderPosition,
+        LayoutColor = LayoutColor
+    };
+
+    public event Action? OnUpdate;
+    
+    public enum LayoutPosition
+    {
+        Left = Anchor.Left,
+        Right = Anchor.Right,
+        Top = Anchor.Top,
+        Bottom = Anchor.Bottom
+    }
+
+    public enum Color
+    {
+        Primary,
+        PrimarySwapped,
+        Secondary,
+        SecondarySwapped,
+        Tertiary,
+        TertiarySwapped,
+    }
+
+    public bool Equals(LayoutSettings? other)
+    {
+        if (other is null) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return _drawerPosition == other._drawerPosition 
+               && _headerPosition == other._headerPosition 
+               && _layoutColor == other._layoutColor 
+               && _closeDrawerOnMainFocus == other._closeDrawerOnMainFocus
+               && _closeDrawerOnNavigate == other._closeDrawerOnNavigate;
+    }
+
+    public override bool Equals(object? obj) => obj is LayoutSettings settings && Equals(settings);
+
+    public override int GetHashCode() => Hash.GetHashCode();
+
+
+    public static bool operator ==(LayoutSettings? left, LayoutSettings? right) => Equals(left, right);
+
+    public static bool operator !=(LayoutSettings? left, LayoutSettings? right) => !Equals(left, right);
 }
---
+++ Displays/ExampleSite/PushOutQueue.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Diagnostics.Contracts;
+
+namespace ExampleSite;
+
+/// <summary>
+/// Represents a first-in, first-out collection that removes the items from the front of the queue to add new items
+/// to the back when the specified item capacity is reached.
+/// </summary>
+public class PushOutQueue<T> : ICollection<T>, IReadOnlyCollection<T>
+{
+    public int MaxItems { get; }
+    private T[] _items;
+    
+    /// <summary>
+    /// Creates a first-in, first-out collection that removes the items from the front of the queue to add new items
+    /// to the back when the specified item capacity is reached.
+    /// </summary>
+    /// <param name="maxItems">The maximum capacity of items allowed in the queue. Must be at least 1.</param>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="maxItems"/> is less than 1.</exception>
+    public PushOutQueue(int maxItems)
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThan(maxItems, 1);
+        MaxItems = maxItems;
+        _items = new T[maxItems];
+    }
+
+    public IEnumerator<T> GetEnumerator() => new Enumerator(this);
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public void Enqueue(T item)
+    {
+        if (Count >= MaxItems) RemoveCount(1);
+        _items[Count] = item;
+        Count += 1;
+    }
+
+    public void Add(T item) => Enqueue(item);
+
+    public void Clear() => _items = [];
+
+    [Pure]
+    public bool Contains(T item) => _items.Contains(item);
+
+    public void CopyTo(T[] array, int arrayIndex) => _items.CopyTo(array, arrayIndex);
+
+    public bool Remove(T item)
+    {
+        int i = Array.IndexOf(_items, item);
+        if (i < 0) return false;
+        return RemoveAt(i);
+    }
+
+    public T Dequeue()
+    {
+        ThrowIfEmptyQueue("Cannot dequeue an element from an empty queue.");
+        T item = _items[0];
+        RemoveCount(1);
+        return item;
+    }
+    
+    public void RemoveCount(int count)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(count);
+        if (count is 0) return;
+        
+        if (count < MaxItems)
+        {
+            T[] source = _items[count..];
+            _items = new T[MaxItems];
+            Count -= count;
+            Array.Copy(source, _items, Count);
+        }
+        else
+        {
+            _items = new T[MaxItems];
+            Count = 0;
+        }
+    }
+
+    public bool RemoveAt(int i)
+    {
+        if (i >= Count || i < 0) return false;
+
+        _items = [.._items[..i], .._items[(i + 1)..]];
+        Count -= 1;
+        return true;
+    }
+
+    public int Count { get; private set; }
+    public bool IsReadOnly => false;
+
+    private void ThrowIfEmptyQueue(string? errorMessage = null)
+    {
+        if (Count is 0) throw new InvalidOperationException($"Queue empty. {errorMessage}");
+    }
+    
+    public PushOutQueue<T> Clone() => new(MaxItems) { _items = _items };
+
+#nullable disable
+    private class Enumerator(PushOutQueue<T> queue) : IEnumerator<T>
+    {
+        private PushOutQueue<T> Queue { get; set; } = queue;
+        private PushOutQueue<T> BackupQueue { get; } = queue.Clone();
+
+        public bool MoveNext()
+        {
+            if (Queue.Count is 0) return false;
+            Current = Queue.Dequeue();
+            return true;
+        }
+
+        public void Reset()
+        {
+            Current = default;
+            Queue = BackupQueue.Clone();
+        }
+
+        public T Current { get; private set; }
+
+        object IEnumerator.Current => Current!;
+
+        public void Dispose()
+        {
+            Array.Clear(BackupQueue._items);
+            Array.Clear(Queue._items);
+        }
+    }
+}
